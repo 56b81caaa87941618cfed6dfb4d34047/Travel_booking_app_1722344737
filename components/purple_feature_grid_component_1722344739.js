00508f35e6e17e6b5c4bb8eecff50eaea5f438c8
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-30 14:39:17 +0000

    Code generation

--- components/purple_feature_grid_component_1722344739.js
+++ components/purple_feature_grid_component_1722344739.js
@@ -1,11 +1,12 @@
 Vue.component("purple_feature_grid_component_1722344739", {
     template: `
     <div class="bg-gradient-to-br from-deep-blue-900 to-purple-900 p-8 flex justify-center items-center rounded-lg shadow-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-full h-auto text-purple-400 filter drop-shadow-lg">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
-                <path d="M16.24 7.76l-1.41 1.41c.5.5.86 1.12 1.05 1.8l1.97-.4c-.27-1.07-.8-2.05-1.61-2.81zM7.76 7.76c-.81.76-1.34 1.74-1.61 2.81l1.97.4c.19-.68.55-1.3 1.05-1.8L7.76 7.76z"/>
-                <path d="M12 16c-1.1 0-2-.9-2-2h4c0 1.1-.9 2-2 2z"/>
-            </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-full h-auto text-purple-400 filter drop-shadow-lg">
+            <path d="M12,2C6.47,2,2,6.47,2,12c0,5.53,4.47,10,10,10s10-4.47,10-10C22,6.47,17.53,2,12,2z M12,20c-4.41,0-8-3.59-8-8s3.59-8,8-8 s8,3.59,8,8S16.41,20,12,20z"/>
+            <path d="M16.5,11c-0.83,0-1.5-0.67-1.5-1.5S15.67,8,16.5,8S18,8.67,18,9.5S17.33,11,16.5,11z"/>
+            <path d="M7.5,11C6.67,11,6,10.33,6,9.5S6.67,8,7.5,8S9,8.67,9,9.5S8.33,11,7.5,11z"/>
+            <path d="M12,18c-2.28,0-4.22-1.66-5-4h10C16.22,16.34,14.28,18,12,18z"/>
+        </svg>
     </div>
     `,
     data() {
